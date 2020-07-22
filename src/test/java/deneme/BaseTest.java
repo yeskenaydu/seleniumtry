784b32b20dec0dcb47c2ee6a6b465d2a592f2301
yeskenaydu <yeskenaydu@gmail.com>
2020-07-22 22:03:39 +0300

    still trying

---
+++ src/test/java/deneme/BaseTest.java
@@ -0,0 +1,29 @@
+package deneme;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    private WebDriver driver;
+
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "kaynaklar/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.trendyol.com");
+        System.out.println((driver.getTitle()));
+
+        Thread.sleep(2000);
+        WebElement carpi = driver.findElement(By.className("fancybox-item fancybox-close"));
+        System.out.println(carpi.getTagName());
+        carpi.click();
+
+        Thread.sleep(2000);
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        BaseTest test = new BaseTest();
+        test.setup();
+    }
+}
